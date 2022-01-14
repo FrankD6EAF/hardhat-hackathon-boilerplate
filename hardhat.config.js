9ac866befbfa073696bf62c7ba462f6a353bf5e6
FrankD6EAF <frankd6eaf@gmail.com>
2022-01-14 18:35:59 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.3"
+  solidity: process.env.SOLIDITY_VERSION || "0.7.3",
+  networks: {
+    [process.env.NETWORK_NAME]: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  }
 };
